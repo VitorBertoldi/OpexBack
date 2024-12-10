9fae612456c9f361d90ce34f9901b07fb650b790
VitorBertoldi <117694686+VitorBertoldi@users.noreply.github.com>
2024-12-10 20:25:40 -0300

    initial commit

---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+const userController = {
+    register: function (req, res) {
+        res.send('register')
+    },
+    login: function (req, res) {
+        res.send('login')
+    }
+
+}
+
+
+module.exports = userController;
---
+++ index.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const userRouter = require('./routes/userRouter');
+
+app.use('/user', userRouter);
+app.listen(process.env.PORT, () => { console.log("Server Running")})
